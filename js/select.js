3c834ff73806093fd8620b0bbd8b5b036534af93
monabberhossain <mn2world@gmail.com>
2022-08-23 06:59:32 +0600

    js file added

---
+++ js/select.js
@@ -0,0 +1,117 @@
+const selectButtonOne = document.getElementById('select-button-1');
+selectButtonOne.addEventListener('click', function() {
+    const playerOneName = document.getElementById('player-1').innerText;
+    const selectedPlayerOne = document.getElementById('selected-player-1');
+    selectedPlayerOne.innerText = playerOneName;
+    selectButtonOne.style.pointerEvents = 'none';
+    selectButtonOne.style.backgroundColor = 'gray';
+});
+
+const selectButtonTwo = document.getElementById('select-button-2');
+selectButtonTwo.addEventListener('click', function() {
+    const playerTwoName = document.getElementById('player-2').innerText;
+    const selectedPlayerTwo = document.getElementById('selected-player-2');
+    selectedPlayerTwo.innerText = playerTwoName;
+    selectButtonTwo.style.pointerEvents = 'none';
+    selectButtonTwo.style.backgroundColor = 'gray';
+});
+
+const selectButtonThree = document.getElementById('select-button-3');
+selectButtonThree.addEventListener('click', function() {
+    const playerThreeName = document.getElementById('player-3').innerText;
+    const selectedPlayerThree = document.getElementById('selected-player-3');
+    selectedPlayerThree.innerText = playerThreeName;
+    selectButtonThree.style.pointerEvents = 'none';
+    selectButtonThree.style.backgroundColor = 'gray';
+});
+
+const selectButtonFour = document.getElementById('select-button-4');
+selectButtonFour.addEventListener('click', function() {
+    const playerFourName = document.getElementById('player-4').innerText;
+    const selectedPlayerFour = document.getElementById('selected-player-4');
+    selectedPlayerFour.innerText = playerFourName;
+    selectButtonFour.style.pointerEvents = 'none';
+    selectButtonFour.style.backgroundColor = 'gray';
+});
+
+const selectButtonFive = document.getElementById('select-button-5');
+selectButtonFive.addEventListener('click', function() {
+    const playerFiveName = document.getElementById('player-5').innerText;
+    const selectedPlayerTwo = document.getElementById('selected-player-2');
+    selectedPlayerTwo.innerText = playerFiveName;
+    selectButtonTwo.style.pointerEvents = 'none';
+    selectButtonTwo.style.backgroundColor = 'gray';
+});
+
+const selectButtonSix = document.getElementById('select-button-6');
+selectButtonSix.addEventListener('click', function() {
+    const playerSixName = document.getElementById('player-6').innerText;
+    const selectedPlayerTwo = document.getElementById('selected-player-2');
+    selectedPlayerTwo.innerText = playerSixName;
+    selectButtonTwo.style.pointerEvents = 'none';
+    selectButtonTwo.style.backgroundColor = 'gray';
+});
+
+const selectButtonSeven = document.getElementById('select-button-7');
+selectButtonSeven.addEventListener('click', function() {
+    const playerSevenName = document.getElementById('player-7').innerText;
+    const selectedPlayerTwo = document.getElementById('selected-player-2');
+    selectedPlayerTwo.innerText = playerTwoName;
+    selectButtonTwo.style.pointerEvents = 'none';
+    selectButtonTwo.style.backgroundColor = 'gray';
+});
+
+const selectButtonEight = document.getElementById('select-button-8');
+selectButtonEight.addEventListener('click', function() {
+    const playerEightName = document.getElementById('player-8').innerText;
+    const selectedPlayerTwo = document.getElementById('selected-player-2');
+    selectedPlayerTwo.innerText = playerTwoName;
+    selectButtonTwo.style.pointerEvents = 'none';
+    selectButtonTwo.style.backgroundColor = 'gray';
+});
+
+const selectButtonNine = document.getElementById('select-button-9');
+selectButtonNine.addEventListener('click', function() {
+    const playerNineName = document.getElementById('player-9').innerText;
+    const selectedPlayerTwo = document.getElementById('selected-player-2');
+    selectedPlayerTwo.innerText = playerTwoName;
+    selectButtonTwo.style.pointerEvents = 'none';
+    selectButtonTwo.style.backgroundColor = 'gray';
+});
+
+const selectButtonTen = document.getElementById('select-button-10');
+selectButtonTen.addEventListener('click', function() {
+    const playerTenName = document.getElementById('player-10').innerText;
+    const selectedPlayerTwo = document.getElementById('selected-player-2');
+    selectedPlayerTwo.innerText = playerTwoName;
+    selectButtonTwo.style.pointerEvents = 'none';
+    selectButtonTwo.style.backgroundColor = 'gray';
+});
+
+const selectButtonEleven = document.getElementById('select-button-11');
+selectButtonEleven.addEventListener('click', function() {
+    const playerElevenName = document.getElementById('player-11').innerText;
+    const selectedPlayerTwo = document.getElementById('selected-player-2');
+    selectedPlayerTwo.innerText = playerTwoName;
+    selectButtonTwo.style.pointerEvents = 'none';
+    selectButtonTwo.style.backgroundColor = 'gray';
+});
+
+const selectButtonTwelve = document.getElementById('select-button-12');
+selectButtonTwelve.addEventListener('click', function() {
+    const playerTwelveName = document.getElementById('player-12').innerText;
+    const selectedPlayerTwo = document.getElementById('selected-player-2');
+    selectedPlayerTwo.innerText = playerTwoName;
+    selectButtonTwo.style.pointerEvents = 'none';
+    selectButtonTwo.style.backgroundColor = 'gray';
+});
+
+// const selectButton = document.getElementById('select-button-1');
+
+// function selectPlayer(buttonPlayer, playerId){
+//     const playerName = document.getElementById(buttonPlayer);
+//     buttonPlayer.addEventListener('click', function () {
+//         const playerName = document.getElementById(playerId).innerText;
+//         const selectedPlayer = document.getElementById('selected-player-1');
+//     })
+// }
