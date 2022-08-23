a33ee21104f4d7d5c4905c17a239453e62dc3d62
monabberhossain <mn2world@gmail.com>
2022-08-23 11:03:45 +0600

    player selection functionality completed

--- js/select.js
+++ js/select.js
@@ -1,117 +1,215 @@
 const selectButtonOne = document.getElementById('select-button-1');
 selectButtonOne.addEventListener('click', function() {
     const playerOneName = document.getElementById('player-1').innerText;
-    const selectedPlayerOne = document.getElementById('selected-player-1');
-    selectedPlayerOne.innerText = playerOneName;
-    selectButtonOne.style.pointerEvents = 'none';
-    selectButtonOne.style.backgroundColor = 'gray';
+    const playerList = document.getElementById("player-list").children;
+    if(playerList.length<5){
+        const lists = document.createElement("li");
+        lists.className = "mb-3";
+        const listItem = document.createTextNode(playerOneName);
+        lists.appendChild(listItem);
+        document.getElementById("player-list").appendChild(lists);
+        selectButtonOne.style.pointerEvents = 'none';
+        selectButtonOne.style.backgroundColor = 'gray';
+    }
+    else{
+        alert('You can not select more than five players');
+    }
 });
 
 const selectButtonTwo = document.getElementById('select-button-2');
 selectButtonTwo.addEventListener('click', function() {
     const playerTwoName = document.getElementById('player-2').innerText;
-    const selectedPlayerTwo = document.getElementById('selected-player-2');
-    selectedPlayerTwo.innerText = playerTwoName;
-    selectButtonTwo.style.pointerEvents = 'none';
-    selectButtonTwo.style.backgroundColor = 'gray';
+    const playerList = document.getElementById("player-list").children;
+    if(playerList.length<5){
+        const lists = document.createElement("li");
+        lists.className = "mb-3";
+        const listItem = document.createTextNode(playerTwoName);
+        lists.appendChild(listItem);
+        document.getElementById("player-list").appendChild(lists);
+        selectButtonTwo.style.pointerEvents = 'none';
+        selectButtonTwo.style.backgroundColor = 'gray';
+    }
+    else{
+        alert('You can not select more than five players');
+    }
 });
 
 const selectButtonThree = document.getElementById('select-button-3');
 selectButtonThree.addEventListener('click', function() {
     const playerThreeName = document.getElementById('player-3').innerText;
-    const selectedPlayerThree = document.getElementById('selected-player-3');
-    selectedPlayerThree.innerText = playerThreeName;
-    selectButtonThree.style.pointerEvents = 'none';
-    selectButtonThree.style.backgroundColor = 'gray';
+    const playerList = document.getElementById("player-list").children;
+    if(playerList.length<5){
+        const lists = document.createElement("li");
+        lists.className = "mb-3";
+        const listItem = document.createTextNode(playerThreeName);
+        lists.appendChild(listItem);
+        document.getElementById("player-list").appendChild(lists);
+        selectButtonThree.style.pointerEvents = 'none';
+        selectButtonThree.style.backgroundColor = 'gray';
+    }
+    else{
+        alert('You can not select more than five players');
+    }
 });
 
 const selectButtonFour = document.getElementById('select-button-4');
 selectButtonFour.addEventListener('click', function() {
     const playerFourName = document.getElementById('player-4').innerText;
-    const selectedPlayerFour = document.getElementById('selected-player-4');
-    selectedPlayerFour.innerText = playerFourName;
-    selectButtonFour.style.pointerEvents = 'none';
-    selectButtonFour.style.backgroundColor = 'gray';
+    const playerList = document.getElementById("player-list").children;
+    if(playerList.length<5){
+        const lists = document.createElement("li");
+        lists.className = "mb-3";
+        const listItem = document.createTextNode(playerFourName);
+        lists.appendChild(listItem);
+        document.getElementById("player-list").appendChild(lists);
+        selectButtonFour.style.pointerEvents = 'none';
+        selectButtonFour.style.backgroundColor = 'gray';
+    }
+    else{
+        alert('You can not select more than five players');
+    }
 });
 
 const selectButtonFive = document.getElementById('select-button-5');
 selectButtonFive.addEventListener('click', function() {
     const playerFiveName = document.getElementById('player-5').innerText;
-    const selectedPlayerTwo = document.getElementById('selected-player-2');
-    selectedPlayerTwo.innerText = playerFiveName;
-    selectButtonTwo.style.pointerEvents = 'none';
-    selectButtonTwo.style.backgroundColor = 'gray';
+    const playerList = document.getElementById("player-list").children;
+    if(playerList.length<5){
+        const lists = document.createElement("li");
+        lists.className = "mb-3";
+        const listItem = document.createTextNode(playerFiveName);
+        lists.appendChild(listItem);
+        document.getElementById("player-list").appendChild(lists);
+        selectButtonFive.style.pointerEvents = 'none';
+        selectButtonFive.style.backgroundColor = 'gray';
+    }
+    else{
+        alert('You can not select more than five players');
+    }
 });
 
 const selectButtonSix = document.getElementById('select-button-6');
 selectButtonSix.addEventListener('click', function() {
     const playerSixName = document.getElementById('player-6').innerText;
-    const selectedPlayerTwo = document.getElementById('selected-player-2');
-    selectedPlayerTwo.innerText = playerSixName;
-    selectButtonTwo.style.pointerEvents = 'none';
-    selectButtonTwo.style.backgroundColor = 'gray';
+    const playerList = document.getElementById("player-list").children;
+    if(playerList.length<5){
+        const lists = document.createElement("li");
+        lists.className = "mb-3";
+        const listItem = document.createTextNode(playerSixName);
+        lists.appendChild(listItem);
+        document.getElementById("player-list").appendChild(lists);
+        selectButtonSix.style.pointerEvents = 'none';
+        selectButtonSix.style.backgroundColor = 'gray';
+    }
+    else{
+        alert('You can not select more than five players');
+    }
 });
 
 const selectButtonSeven = document.getElementById('select-button-7');
 selectButtonSeven.addEventListener('click', function() {
     const playerSevenName = document.getElementById('player-7').innerText;
-    const selectedPlayerTwo = document.getElementById('selected-player-2');
-    selectedPlayerTwo.innerText = playerTwoName;
-    selectButtonTwo.style.pointerEvents = 'none';
-    selectButtonTwo.style.backgroundColor = 'gray';
+    const playerList = document.getElementById("player-list").children;
+    if(playerList.length<5){
+        const lists = document.createElement("li");
+        lists.className = "mb-3";
+        const listItem = document.createTextNode(playerSevenName);
+        lists.appendChild(listItem);
+        document.getElementById("player-list").appendChild(lists);
+        selectButtonSeven.style.pointerEvents = 'none';
+        selectButtonSeven.style.backgroundColor = 'gray';
+    }
+    else{
+        alert('You can not select more than five players');
+    }
 });
 
 const selectButtonEight = document.getElementById('select-button-8');
 selectButtonEight.addEventListener('click', function() {
     const playerEightName = document.getElementById('player-8').innerText;
-    const selectedPlayerTwo = document.getElementById('selected-player-2');
-    selectedPlayerTwo.innerText = playerTwoName;
-    selectButtonTwo.style.pointerEvents = 'none';
-    selectButtonTwo.style.backgroundColor = 'gray';
+    const playerList = document.getElementById("player-list").children;
+    if(playerList.length<5){
+        const lists = document.createElement("li");
+        lists.className = "mb-3";
+        const listItem = document.createTextNode(playerEightName);
+        lists.appendChild(listItem);
+        document.getElementById("player-list").appendChild(lists);
+        selectButtonEight.style.pointerEvents = 'none';
+        selectButtonEight.style.backgroundColor = 'gray';
+    }
+    else{
+        alert('You can not select more than five players');
+    }
 });
 
 const selectButtonNine = document.getElementById('select-button-9');
 selectButtonNine.addEventListener('click', function() {
     const playerNineName = document.getElementById('player-9').innerText;
-    const selectedPlayerTwo = document.getElementById('selected-player-2');
-    selectedPlayerTwo.innerText = playerTwoName;
-    selectButtonTwo.style.pointerEvents = 'none';
-    selectButtonTwo.style.backgroundColor = 'gray';
+    const playerList = document.getElementById("player-list").children;
+    if(playerList.length<5){
+        const lists = document.createElement("li");
+        lists.className = "mb-3";
+        const listItem = document.createTextNode(playerNineName);
+        lists.appendChild(listItem);
+        document.getElementById("player-list").appendChild(lists);
+        selectButtonNine.style.pointerEvents = 'none';
+        selectButtonNine.style.backgroundColor = 'gray';
+    }
+    else{
+        alert('You can not select more than five players');
+    }
 });
 
 const selectButtonTen = document.getElementById('select-button-10');
 selectButtonTen.addEventListener('click', function() {
     const playerTenName = document.getElementById('player-10').innerText;
-    const selectedPlayerTwo = document.getElementById('selected-player-2');
-    selectedPlayerTwo.innerText = playerTwoName;
-    selectButtonTwo.style.pointerEvents = 'none';
-    selectButtonTwo.style.backgroundColor = 'gray';
+    const playerList = document.getElementById("player-list").children;
+    if(playerList.length<5){
+        const lists = document.createElement("li");
+        lists.className = "mb-3";
+        const listItem = document.createTextNode(playerTenName);
+        lists.appendChild(listItem);
+        document.getElementById("player-list").appendChild(lists);
+        selectButtonTen.style.pointerEvents = 'none';
+        selectButtonTen.style.backgroundColor = 'gray';
+    }
+    else{
+        alert('You can not select more than five players');
+    }
 });
 
 const selectButtonEleven = document.getElementById('select-button-11');
 selectButtonEleven.addEventListener('click', function() {
     const playerElevenName = document.getElementById('player-11').innerText;
-    const selectedPlayerTwo = document.getElementById('selected-player-2');
-    selectedPlayerTwo.innerText = playerTwoName;
-    selectButtonTwo.style.pointerEvents = 'none';
-    selectButtonTwo.style.backgroundColor = 'gray';
+    const playerList = document.getElementById("player-list").children;
+    if(playerList.length<5){
+        const lists = document.createElement("li");
+        lists.className = "mb-3";
+        const listItem = document.createTextNode(playerElevenName);
+        lists.appendChild(listItem);
+        document.getElementById("player-list").appendChild(lists);
+        selectButtonEleven.style.pointerEvents = 'none';
+        selectButtonEleven.style.backgroundColor = 'gray';
+    }
+    else{
+        alert('You can not select more than five players');
+    }
 });
 
 const selectButtonTwelve = document.getElementById('select-button-12');
 selectButtonTwelve.addEventListener('click', function() {
     const playerTwelveName = document.getElementById('player-12').innerText;
-    const selectedPlayerTwo = document.getElementById('selected-player-2');
-    selectedPlayerTwo.innerText = playerTwoName;
-    selectButtonTwo.style.pointerEvents = 'none';
-    selectButtonTwo.style.backgroundColor = 'gray';
+    const playerList = document.getElementById("player-list").children;
+    if(playerList.length<5){
+        const lists = document.createElement("li");
+        lists.className = "mb-3";
+        const listItem = document.createTextNode(playerTwelveName);
+        lists.appendChild(listItem);
+        document.getElementById("player-list").appendChild(lists);
+        selectButtonTwelve.style.pointerEvents = 'none';
+        selectButtonTwelve.style.backgroundColor = 'gray';
+    }
+    else{
+        alert('You can not select more than five players');
+    }
 });
-
-// const selectButton = document.getElementById('select-button-1');
-
-// function selectPlayer(buttonPlayer, playerId){
-//     const playerName = document.getElementById(buttonPlayer);
-//     buttonPlayer.addEventListener('click', function () {
-//         const playerName = document.getElementById(playerId).innerText;
-//         const selectedPlayer = document.getElementById('selected-player-1');
-//     })
-// }
